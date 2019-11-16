c575f795f839a43c3af11f77aab382a8faeca871
prachi <malusare.prachi@gmail.com>
2019-11-16 20:20:10 +0530

    final review

---
+++ assets/script/main.js
@@ -0,0 +1,12 @@
+// $(document).ready(function(){
+//     $('.btn-more').click(function(){
+//         $('.show-more').toggleClass('active');
+//     });
+// });
+
+
+$(document).ready(function(){
+    $('.image').hover(function(){
+        $(this).attr('src','../images/cut-img.png');
+    });
+});
